61fdde9f5b9686168b9cd570813cf15da99ed638
Rasmus Bach Jørgensen <rasm74e5@edu.eal.dk>
2019-05-10 11:03:09 +0200

    GetBoardgame er færdig for nu.

--- Papas_System/Application/BoardgameRepository.cs
+++ Papas_System/Application/BoardgameRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace Papas_System.Application
 {
@@ -12,86 +13,85 @@ namespace Papas_System.Application
 
         public void AddBoardgame()
         {
-            
-                bool runWhileTrue = true;
-                while (runWhileTrue)
+
+            bool runWhileTrue = true;
+            while (runWhileTrue)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Tilføj nyt brætspil");
+                Console.WriteLine("2. Vis alle brætspil");
+                int menuInput = int.Parse(Console.ReadLine());
+                switch (menuInput)
                 {
-                    Console.Clear();
-                    Console.WriteLine("1. Tilføj nyt brætspil");
-                    Console.WriteLine("2. Vis alle brætspil");
-                    int menuInput = int.Parse(Console.ReadLine());
-                    switch (menuInput)
-                    {
-                        case 0:
-                            runWhileTrue = false;
-                            break;
-                        case 1:
-                            Console.WriteLine("Tilføj nyt brætspil");
-                            Console.Write("Indtast navn på brætspil: ");
-                            string boardgameName = Console.ReadLine();
-                            Console.Write("Anbefalet antal af spillere: ");
-                            string numberOfPlayers = Console.ReadLine();
-                            Console.Write("Anbefalet aldersgruppe: ");
-                            string audience = Console.ReadLine();
-                            Console.Write("Forventet spilletid: ");
-                            string expectedGameTime = Console.ReadLine();
-                            Console.Write("Distributør: ");
-                            string distributor =Console.ReadLine();
-                            //Console.Write("Brætspils Id: ");
-                            //int boardgameId = int.Parse(Console.ReadLine());
-                            Console.Write("Spilgenre: ");
-                            string gameTag = Console.ReadLine();
+                    case 0:
+                        runWhileTrue = false;
+                        break;
+                    case 1:
+                        Console.WriteLine("Tilføj nyt brætspil");
+                        Console.Write("Indtast navn på brætspil: ");
+                        string boardgameName = Console.ReadLine();
+                        Console.Write("Anbefalet antal af spillere: ");
+                        string numberOfPlayers = Console.ReadLine();
+                        Console.Write("Anbefalet aldersgruppe: ");
+                        string audience = Console.ReadLine();
+                        Console.Write("Forventet spilletid: ");
+                        string expectedGameTime = Console.ReadLine();
+                        Console.Write("Distributør: ");
+                        string distributor = Console.ReadLine();
+                        //Console.Write("Brætspils Id: ");
+                        //int boardgameId = int.Parse(Console.ReadLine());
+                        Console.Write("Spilgenre: ");
+                        string gameTag = Console.ReadLine();
 
                         InsertBoardgame(boardgameName, numberOfPlayers, audience, expectedGameTime, distributor, /*boardgameId,*/ gameTag);
-                            Console.Clear();
-                            break;
-                        case 2:
-                            Console.WriteLine("Viser alle brætspil");
-                            //GetBoardgame(); SKal implementeres længere nede
-                            Console.Clear();
-                            break;
-                        default:
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.WriteLine("Viser alle brætspil");
+                        //GetBoardgame(); SKal implementeres længere nede
+                        Console.Clear();
+                        break;
+                    default:
 
-                            break;
-                    }
+                        break;
                 }
+            }
 
         }
 
-            public void InsertBoardgame(string boardgameName, string numberOfPlayers, string audience, string expectedGameTime, string distributor, /*int boardgameId,*/ string gameTag)
-            {
+        public void InsertBoardgame(string boardgameName, string numberOfPlayers, string audience, string expectedGameTime, string distributor, /*int boardgameId,*/ string gameTag)
+        {
 
             using (SqlConnection con = new SqlConnection(connectionString))
             {
-                    string query = "INSERT INTO Game_Libary (Boardgame_Name, Player_Count, Audience, Game_Time, Distributor, Boardgame_Id , GameTag) VALUES " +
-                        "(@Boardgame_Name, @Player_Count, @Audience, @Game_Time, @Distributor, @Boardgame_Id, @GameTag)";
-                
+                string query1 = "INSERT INTO Game_Libary (Boardgame_Name, Player_Count, Audience, Game_Time, Distributor, Boardgame_Id , GameTag) VALUES " +
+                    "(@Boardgame_Name, @Player_Count, @Audience, @Game_Time, @Distributor, @Boardgame_Id, @GameTag)";
 
-                using (SqlCommand command = new SqlCommand(query, con))
+
+                using (SqlCommand command = new SqlCommand(query1, con))
+                {
+                    try
                     {
-                        try
-                        {
-                            command.Parameters.Add(new SqlParameter("@Boardgame_Name", boardgameName));
-                            command.Parameters.Add(new SqlParameter("@Player_Count", numberOfPlayers));                            
-                            command.Parameters.Add(new SqlParameter("@Audience", audience));
-                            command.Parameters.Add(new SqlParameter("@Game_Time", expectedGameTime));
-                            command.Parameters.Add(new SqlParameter("@Distributor",distributor));
-                            command.Parameters.Add(new SqlParameter("@Boardgame_Id", 1));
-                            command.Parameters.Add(new SqlParameter("@GameTag", gameTag));
+                        command.Parameters.Add(new SqlParameter("@Boardgame_Name", boardgameName));
+                        command.Parameters.Add(new SqlParameter("@Player_Count", numberOfPlayers));
+                        command.Parameters.Add(new SqlParameter("@Audience", audience));
+                        command.Parameters.Add(new SqlParameter("@Game_Time", expectedGameTime));
+                        command.Parameters.Add(new SqlParameter("@Distributor", distributor));
+                        command.Parameters.Add(new SqlParameter("@Boardgame_Id", 1));
+                        command.Parameters.Add(new SqlParameter("@GameTag", gameTag));
 
                         con.Open();
-                            command.ExecuteNonQuery();
-                            con.Close();
+                        command.ExecuteNonQuery();
+                        con.Close();
 
-                            Console.WriteLine("Brætspil er tilføjet");
-                            Console.ReadLine();
-                        }
+                        Console.WriteLine("Brætspil er tilføjet");
+                        Console.ReadLine();
+                    }
                     catch (Exception e)
                     {
                         Console.WriteLine(e.Message);
                         Console.ReadLine();
                     }
-                }
                 }
             }
         }
@@ -99,46 +99,48 @@ namespace Papas_System.Application
         //{
 
         //}
-        //public void GetBoardgame()
-        //{
-        //using (SqlConnection con = new SqlConnection(connectionString))
-        //{
-        //    string query = "SELECT * FROM PET";
-        //    using (SqlCommand command = new SqlCommand(query, con))
-        //    {
-        //        try
-        //        {
-        //            con.Open();
-        //            SqlDataReader reader = command.ExecuteReader();
+        public void GetBoardgame()
+        {
 
-        //            if (reader.HasRows)
-        //            {
-        //                while (reader.Read())
-        //                {
-        //                    int PetID = int.Parse(reader["PetID"].ToString());
-        //                    string PetName = reader["PetName"].ToString();
-        //                    string PetType = reader["PetType"].ToString();
-        //                    string PetBreed = reader["PetBreed"].ToString();
-        //                    string PetDOBl = reader["PetDOBl"].ToString();
-        //                    string petWeight = reader["petWeight"].ToString();
-        //                    int OwnerID = int.Parse(reader["OwnerID"].ToString());
-        //                    Console.WriteLine(PetID + ", " + PetName + ", " + PetType + ", " + PetBreed + ", " + PetDOBl + ", " + petWeight + ", " + OwnerID);
-        //                }
-        //            }
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query2 = "SELECT" +
+                    "(@Boardgame_Name, @Player_Count, @Audience, @Game_Time, @Distributor, @Boardgame_Id, @GameTag)";
+                try
+                {
+                    con.Open();
 
-        //            con.Close();
+                    SqlCommand cmd2 = new SqlCommand(query2, con);
+                    cmd2.CommandType = CommandType.StoredProcedure;
 
-        //            Console.ReadLine();
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            Console.WriteLine(e.Message);
-        //            Console.ReadLine();
-        //        }
-        //    }
-        //}
+                    SqlDataReader reader = cmd2.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+
+                            string boardgameName = reader["Boardgame_Name"].ToString();
+                            string numberOfPlayers = reader["Player_Count"].ToString();
+                            string audience = reader["Audience"].ToString();
+                            string expectedGameTime = reader["Game_Time"].ToString();
+                            string distributor = reader["Distributor"].ToString();
+                            string boardgameId = reader["Boardgame_Id"].ToString();
+                            string gameTag = reader ["GameTag"].ToString();
+                            Console.WriteLine($"\nBoardgame_Name: {boardgameName} \nPlayer_Count: {numberOfPlayers} \nAudience: {audience} " +
+                            $"\nGame_Time: {expectedGameTime} \nDistributor: {distributor}\nBoardgame_Id: {boardgameId}\nGameTag {gameTag}\n");
+                        }
+                    }
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Fejl: " + e.Message);
+                }
+            }
+        }
 
     }
+}
         //public ModifyBoardgame()
         //{
 
